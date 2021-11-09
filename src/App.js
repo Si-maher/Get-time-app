83ad9104534a7e9dcad18ea572b719e8c1afc118
Simon Maher <simonmaher8307@hotmail.com>
2021-11-09 11:27:28 +0200

    created state, function to render time, set interval

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, {useState} from 'react'
 import './App.css';
 
 function App() {
-
   const now = new Date().toLocaleTimeString()
   const [time, setTime] = useState(now)
-
+  
+  const upDateTime = () => {
+    const newTime = new Date().toLocaleTimeString()
+    setTime(newTime)
+  }
+  setInterval(upDateTime, 1000)
   return (
     <div className="container">
-  <h1>{now}</h1>
-  <button>Get Time</button>
+  <h1>{time}</h1>
+  <button onClick = {upDateTime}>Get Time</button>
     </div>
   );
 }
