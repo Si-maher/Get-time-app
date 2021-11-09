31028d51a6b0ae840c4b9adc846625178a674c44
Simon Maher <simonmaher8307@hotmail.com>
2021-11-09 11:00:18 +0200

    added css styles, created useState hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+
+  const now = new Date().toLocaleTimeString()
+  const [time, setTime] = useState(now)
+
   return (
-    <div className="App">
-  
+    <div className="container">
+  <h1>{now}</h1>
+  <button>Get Time</button>
     </div>
   );
 }
